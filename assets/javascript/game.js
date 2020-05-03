d00f050687eb725fb02cdb21d9cd4c27a65e89cb
Tyree West <tyreew594@gmail.com>
2020-05-02 21:36:43 -0500

    rough draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,104 @@
-var wins
+$(document).ready(function() {
+    // Empty Variable
+    var wins = 0;
+    var lose = 0;
+    var totalScore = 0;
+    // Random Number 
+    var computerGuess = parseInt(Math.floor(Math.random() * 110));
+ $('#pointstoreach').html(computerGuess);
+    console.log(computerGuess);
+
+    var ruby = parseInt(Math.floor(2));
+    var topaz = parseInt(Math.floor(6));
+    var memorial = parseInt(Math.floor(1));
+    var emerald = parseInt(Math.floor(4));
+
+    console.log(ruby);
+    console.log(topaz);
+    console.log(memorial);
+    console.log(emerald);
+    // Reset Function
+
+    function reset() {
+        totalScore = 0;
+        $('totalplayerscore').html(totalScore);
+        computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+        $('#pointstoreach').html(computerGuess);
+    
+    ruby = parseInt(Math.floor(2));
+    topaz = parseInt(Math.floor(6));
+    memorial = parseInt(Math.floor(1));
+    emerald = parseInt(Math.floor(4));
+
+    }
+
+
+//create a click function for images generate number
+
+$("#ruby").on("click", function() {
+    
+            totalScore = totalScore + ruby;
+            $("#totalplayerscore").html(totalScore);
+            console.log(ruby + totalScore);
+            if (totalScore == computerGuess) {
+                checkifwon();
+            }else if (totalScore > computerGuess) {
+                checkifwon();
+            }
+});
+    
+$("#topaz").on("click", function() {
+    
+            totalScore = totalScore + topaz;
+            $("#totalplayerscore").html(totalScore);
+            console.log(topaz + totalScore);
+            if (totalScore == computerGuess) {
+                checkifwon();
+            }else if (totalScore > computerGuess) {
+                checkifwon();
+            }
+});
+    
+$("#memorial").on("click", function() {
+    
+            totalScore = totalScore + memorial;
+            $("#totalplayerscore").html(totalScore);
+            console.log(memorial + totalScore);
+            if (totalScore == computerGuess) {
+                checkifwon();
+            }else if (totalScore > computerGuess) {
+                checkifwon();
+            }
+});
+
+$("#emerald").on("click", function() {
+    
+            totalScore = totalScore + emerald;
+            $("#totalplayerscore").html(totalScore);
+            console.log(emerald + totalScore);
+            if (totalScore == computerGuess) {
+                checkifwon();
+            }else if (totalScore > computerGuess) {
+                checkifwon();
+            }
+});
+    // create win and losses
+
+function checkifwon() {
+    
+    if(totalScore === computerGuess) {
+    
+    wins++;
+    console.log(wins);
+    $("#totalscore").html("You Win");
+    $("#wins").html("Wins: " + wins);
+    reset();
+} else if (totalScore > computerGuess) {
+    
+    lose++;
+    console.log(lose);
+    $("#totalscore").html("You Lost");
+    $("#losses").html("Losses: " + lose);
+    reset();
+}
+}});
---
+++ pseudo.md
@@ -0,0 +1,9 @@
+# What happens when the player press an image/button
+* Create a function that has an .onclick attribute so that another function will give the player the amount of points earned on pressing an image.
+# What happens when the button is presses
+* A function that will run a string as a number that will be added to the total score. Also the number that is the scored need to be displayed on the image.
+# What happen when player total points equal or goes over allowable score.
+* need a Variable that hold the points for the player to reach.
+* need a function that when the player hits or passes max points you need if statement that when the number equals max points it gives player a win else give player a loss.
+# reseting the game when player wins or lose.
+* free
